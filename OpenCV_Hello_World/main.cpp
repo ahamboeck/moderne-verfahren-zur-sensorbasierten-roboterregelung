39d2946d3dbad0fb8710205c8525acf53e190846
Alexander Hamboeck <a.hamboeck@gmail.com>
2024-02-24 21:30:22 +0000

    Add main.cpp and Makefile

---
+++ OpenCV_Hello_World/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string.h>
+#include <opencv2/opencv.hpp>l
+
+int main(int argc, char *argv[])
+{   
+    if (argv[1] == NULL)
+    {
+        std::cout << "Please provide an image path" << std::endl;
+        return 1;
+    }
+    std::string imagePath = argv[1];
+    std::cout << "Image path: " << imagePath << std::endl;
+
+    cv::Mat image = cv::imread(imagePath);
+    cv::namedWindow("Hello World", 0);
+    cv::imshow("Hello World", image);
+    cv::waitKey(0);
+    return 0;
+}
