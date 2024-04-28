ef616d3b178da7dfcce63574df29210edf1d62ea
Alexander Hamböck <a.hamboeck@gmail.com>
2024-04-28 17:27:01 +0000

    run_camera_calib.sh: Update script

--- Homework_3/run_camera_calib.sh
+++ Homework_3/run_camera_calib.sh
@@ -1,23 +1,16 @@
 #!/bin/bash
 
-# Start roscore in the background
-roscore &
-roscore_pid=$!
+# Start roscore in a new xterm window
+xterm -hold -e "roscore" &
 
 # Wait for roscore to initialize
 sleep 2
 
-# Start the USB camera node in the background
-roslaunch usb_cam usb_cam-test.launch &
-usb_cam_pid=$!
+# Start the USB camera node in a new xterm window
+xterm -hold -e "roslaunch usb_cam usb_cam-test.launch" &
 
 # Wait for the camera to initialize
 sleep 2
 
-# Start the camera calibration in the background
-rosrun camera_calibration cameracalibrator.py --size 8x6 --square 0.108 image:=/camera/image_raw camera:=/camera --no-service-check &
-calibration_pid=$!
-
-# Optional: Wait for a user input to kill all background processes before the script exits
-read -p "Press any key to terminate all running processes..."
-kill $roscore_pid $usb_cam_pid $calibration_pid
+# Start the camera calibration in a new xterm window
+xterm -hold -e "rosrun camera_calibration cameracalibrator.py --size 8x6 --square 0.108 image:=/camera/image_raw camera:=/camera --no-service-check" &
