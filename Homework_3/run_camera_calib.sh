b34ba6ed69c1e7a54bdba188272340116c4633fb
Alexander Hamboeck <a.hamboeck@gmail.com>
2024-04-29 20:58:24 +0000

    Add camera calibration data

--- Homework_3/run_camera_calib.sh
+++ Homework_3/run_camera_calib.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-# Start roscore in a new xterm window
-xterm -hold -e "roscore" &
+# Start roscore in a new gnome-terminal window
+gnome-terminal -- bash -c "roscore; exec bash"
 
 # Wait for roscore to initialize
 sleep 2
 
-# Start the USB camera node in a new xterm window
-xterm -hold -e "roslaunch usb_cam usb_cam-test.launch" &
+# Start the USB camera node in a new gnome-terminal window
+gnome-terminal -- bash -c "roslaunch usb_cam usb_cam-test.launch; exec bash"
 
 # Wait for the camera to initialize
 sleep 2
 
-# Start the camera calibration in a new xterm window
-xterm -hold -e "rosrun camera_calibration cameracalibrator.py --size 8x6 --square 0.108 image:=/camera/image_raw camera:=/camera --no-service-check" &
+# Start the camera calibration in a new gnome-terminal window
+gnome-terminal -- bash -c "rosrun camera_calibration cameracalibrator.py --size 9x6 --square 0.025 image:=/camera/image_raw camera:=/camera --no-service-check; exec bash"
